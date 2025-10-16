992ab83d84548fbac2379c162d7cf1a8b7ca7db3
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2025-10-16 10:57:03 +0500

    stfu

--- source/game/scripting/FunkinHScript.hx
+++ source/game/scripting/FunkinHScript.hx
@@ -52,6 +52,7 @@ class FunkinHScript extends FunkinRScript
     public static dynamic function getHScriptPreprocessors() {
         var preprocessors:Map<String, Dynamic> = new Map();
         
+        preprocessors.set("mobile", #if mobile true #else false #end);
         preprocessors.set("ENGINE_VER", Application.current.meta.get('version'));
         
         var staticDefines = game.backend.utils.MacroUtil.defines;
